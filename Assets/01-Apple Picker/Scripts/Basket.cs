de45c54c6e0ebc79e24db21a68cb09c29cca4aad
iwcoder98 <iwcoder98@gmail.com>
2024-02-04 16:06:44 -0500

    Fixed Text Objects
    Fixed the text objects by replacing the TextMeshPro components with regular Text components. Added the scripts for the score counter and functionality for removing baskets when apples are missed.

--- Assets/01-Apple Picker/Scripts/Basket.cs
+++ Assets/01-Apple Picker/Scripts/Basket.cs
@@ -3,18 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Basket : MonoBehaviour
-{
+public class Basket : MonoBehaviour {
+
     [Header("Set Dynamically")]
 
-    public Text scoreGT;
+    public Text           scoreGT;
 
-    // Start is called before the first frame update
-    void Start()
-    {
+
+
+    void Start() {
+
+        // Find a reference to the ScoreCounter GameObject
+
         GameObject scoreGO = GameObject.Find("ScoreCounter");
+
+        // Get the Text Component of that GameObject
+
         scoreGT = scoreGO.GetComponent<Text>();
+
+        // Set the starting number of points to 0
+
         scoreGT.text = "0";
+
     }
 
     // Update is called once per frame
@@ -36,6 +46,10 @@ public class Basket : MonoBehaviour
             int score = int.Parse( scoreGT.text );
             score += 100;
             scoreGT.text = score.ToString();
+
+            if (score > HighScore.score) {
+                HighScore.score = score;
+            }
 		}
     }
 }
