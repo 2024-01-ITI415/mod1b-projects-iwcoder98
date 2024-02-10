725b46ecf1f278bbe04188770891d43cfaeb8786
iwcoder98 <iwcoder98@gmail.com>
2024-02-10 11:32:39 -0500

    Finish
    Added level progression.

--- Assets/02-Mission Demolition/Scripts/Slingshot.cs
+++ Assets/02-Mission Demolition/Scripts/Slingshot.cs
@@ -146,6 +146,10 @@ public class Slingshot : MonoBehaviour {
 
             projectile = null;
 
+            MissionDemolition.ShotFired();                             // a
+
+            ProjectileLine.S.poi = projectile;  
+
         }
 
     }
